e9a794e85244d0fad4856b6e53e369d774bbe23c
Martin Prinz <martin.prinz@maibornwolff.de>
2023-05-24 14:10:11 +0200

    fix frame

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -10,7 +10,7 @@ export const Frame = ({
   return (
     <div className="w-full h-full p-6 shadow-frame-outer bg-gradient-to-tr from-gray-600 to-gray-800 dark:from-gray-600 dark:to-gray-800">
       <div className="relative w-full h-full shadow-frame bg-white">
-        <div className="absolute p-10">{children}</div>
+        <div className="absolute w-full h-full p-10">{children}</div>
         {isLoading && <Skeleton className="absolute p-10" />}
       </div>
     </div>
