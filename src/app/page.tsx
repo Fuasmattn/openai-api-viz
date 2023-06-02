71aecb5d86ab1eeff5ec54eddee8fedf0da15b75
Martin Prinz <martin.prinz@maibornwolff.de>
2023-06-02 18:03:16 +0200

    chore: (wip) grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,10 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { useProcessVisualization } from "../context/ProcessVisualizationContext";
-import { Frame } from "../components/Frame";
 import { Prompt } from "../components/prompt/Prompt";
-import {
-  PromptHistory,
-  PromptHistoryItem,
-} from "../components/prompt/PromptHistory";
+import { PromptHistoryItem } from "../components/prompt/PromptHistory";
 
 export default function Landing() {
   const {
@@ -30,7 +27,23 @@ export default function Landing() {
       url: "/default.jpg",
       prompt: "Colorful cartoon portrait of stevie wonder",
     },
+    {
+      url: "/default.jpg",
+      prompt: "Colorful cartoon portrait of stevie wonder",
+    },
+    {
+      url: "/default.jpg",
+      prompt: "Colorful cartoon portrait of stevie wonder",
+    },
+    {
+      url: "/default.jpg",
+      prompt: "Colorful cartoon portrait of stevie wonder",
+    },
   ]);
+
+  const thumbs = urlList.length === 1 ? [] : urlList;
+  const gridCols = urlList.length;
+  const gridRows = (urlList.length % 5) * gridCols;
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -103,11 +116,15 @@ export default function Landing() {
 
   return (
     <main className="dark:bg-slate-900 bg-slate-100 min-h-screen py-16 flex justify-center">
-      <div>
+      <div className="px-10">
         <h2 className="dark:text-white text-2xl mb-10 text-center">
           Create your poster
         </h2>
-        <div className="relative flex flex-row">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="relative flex flex-row"
+        >
           <Prompt
             isLoading={isLoading}
             isRecording={isRecording}
@@ -116,33 +133,47 @@ export default function Landing() {
             handleSubmit={handleSubmit}
             onStartRecording={onClickRecordStart}
           />
-        </div>
+        </motion.div>
 
-        <div className="relative mt-10 w-[512px] h-[512px]">
-          <Frame isLoading={isLoading}>
-            <div className="w-full h-full border-2 border-b-gray-200 border-r-gray-200 border-t-gray-400 border-l-gray-400">
-              {imageUrl && (
-                <Image
-                  className="border-2 border-b-gray-200 border-r-gray-200 border-t-gray-400 border-l-gray-400"
-                  src={imageUrl}
-                  width={512}
-                  height={512}
-                  onLoadingComplete={onLoadingComplete}
-                  alt={prompt ?? "generated poster image"}
-                />
-              )}
-            </div>
-          </Frame>
-        </div>
-        <div className="mt-10">
-          <PromptHistory
-            items={urlList}
-            onSelect={(item: PromptHistoryItem) => {
-              setImageUrl(item.url);
-              setPrompt(item.prompt);
-            }}
-          />
-        </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className={`relative mt-10 grid grid-cols-${gridCols} grid-rows-${gridRows} gap-2 justfy-center`}
+        >
+          {thumbs.map((item) => (
+            <Image
+              key={item.url + "thumb"}
+              className="rounded-lg hover:cursor-pointer hover:shadow-lg brightness-75 hover:brightness-100 transition-all"
+              src={item.url}
+              width={128}
+              height={128}
+              alt={item.prompt}
+              onClick={() => {
+                setImageUrl(item.url);
+                setPrompt(item.prompt);
+              }}
+            />
+          ))}
+
+          {imageUrl && (
+            <Image
+              className={`col-span-${Math.max(
+                1,
+                gridCols - 2
+              )} row-span-${Math.max(0, gridRows - 2)} col-start-${Math.max(
+                1,
+                gridCols - 2
+              )} row-start-${Math.max(0, gridRows - 2)} rounded-lg ${
+                isLoading && "animate-pulse"
+              }`}
+              src={imageUrl}
+              width={400}
+              height={512}
+              onLoadingComplete={onLoadingComplete}
+              alt={prompt ?? "generated poster image"}
+            />
+          )}
+        </motion.div>
       </div>
     </main>
   );
--- src/components/prompt/Prompt.tsx
+++ src/components/prompt/Prompt.tsx
@@ -18,66 +18,64 @@ export const Prompt = ({
   onStartRecording,
 }: Props) => {
   return (
-    <form className="flex gap-2" onSubmit={handleSubmit}>
-      <div className="w-[512px]">
-        <div className="relative w-full">
-          <input
-            disabled={isRecording || isLoading}
-            type="text"
-            id="prompt"
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            className="h-14 pr-24 bg-white border-2 border-gray-900 text-gray-900 text-md rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
-            placeholder="Colorful cartoon portrait of Stevie Wonder"
-            required
-            data-1p-ignore
-          />
-          <input className="opacity-0" type="submit"></input>
+    <form className="flex gap-2 w-full" onSubmit={handleSubmit}>
+      <div className="relative w-full">
+        <input
+          disabled={isRecording || isLoading}
+          type="text"
+          id="prompt"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className="h-14 pl-4 pr-24 bg-white shadow-xl transition-shadow focus:shadow-none focus:border-2 border-0 border-gray-900 text-gray-900 text-md rounded-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
+          placeholder="Colorful cartoon portrait of Stevie Wonder"
+          required
+          data-1p-ignore
+        />
+        <input className="opacity-0" type="submit"></input>
 
-          <div className="absolute top-0 right-0">
-            {!isRecording && (
-              <button
-                disabled={isRecording || isLoading}
-                onClick={onStartRecording}
-                className="hover:cursor-pointer p-4 pr-5 flex justify-center items-center text-pink-500 hover:text-pink-700 active:text-pink-400"
-              >
-                <svg
-                  className="h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                >
-                  <path d="M7 4a3 3 0 016 0v6a3 3 0 11-6 0V4z"></path>
-                  <path d="M5.5 9.643a.75.75 0 00-1.5 0V10c0 3.06 2.29 5.585 5.25 5.954V17.5h-1.5a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-1.5v-1.546A6.001 6.001 0 0016 10v-.357a.75.75 0 00-1.5 0V10a4.5 4.5 0 01-9 0v-.357z"></path>
-                </svg>
-              </button>
-            )}
-          </div>
-          {isRecording && (
-            <motion.div
-              animate={{ rotate: [0, 360] }}
-              transition={{
-                duration: 4,
-                repeat: Infinity,
-              }}
-              className="pointer-events-none absolute text-pink-500 top-4 right-5 w-6 h-6 rounded-full"
+        <div className="absolute top-0 right-0">
+          {!isRecording && (
+            <button
+              disabled={isRecording || isLoading}
+              onClick={onStartRecording}
+              className="hover:cursor-pointer p-4 pr-5 flex justify-center items-center text-pink-500 hover:text-pink-700 active:text-pink-400"
             >
               <svg
+                className="h-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
               >
-                <path
-                  clipRule="evenodd"
-                  fillRule="evenodd"
-                  d="M8.34 1.804A1 1 0 019.32 1h1.36a1 1 0 01.98.804l.295 1.473c.497.144.971.342 1.416.587l1.25-.834a1 1 0 011.262.125l.962.962a1 1 0 01.125 1.262l-.834 1.25c.245.445.443.919.587 1.416l1.473.294a1 1 0 01.804.98v1.361a1 1 0 01-.804.98l-1.473.295a6.95 6.95 0 01-.587 1.416l.834 1.25a1 1 0 01-.125 1.262l-.962.962a1 1 0 01-1.262.125l-1.25-.834a6.953 6.953 0 01-1.416.587l-.294 1.473a1 1 0 01-.98.804H9.32a1 1 0 01-.98-.804l-.295-1.473a6.957 6.957 0 01-1.416-.587l-1.25.834a1 1 0 01-1.262-.125l-.962-.962a1 1 0 01-.125-1.262l.834-1.25a6.957 6.957 0 01-.587-1.416l-1.473-.294A1 1 0 011 10.68V9.32a1 1 0 01.804-.98l1.473-.295c.144-.497.342-.971.587-1.416l-.834-1.25a1 1 0 01.125-1.262l.962-.962A1 1 0 015.38 3.03l1.25.834a6.957 6.957 0 011.416-.587l.294-1.473zM13 10a3 3 0 11-6 0 3 3 0 016 0z"
-                ></path>
+                <path d="M7 4a3 3 0 016 0v6a3 3 0 11-6 0V4z"></path>
+                <path d="M5.5 9.643a.75.75 0 00-1.5 0V10c0 3.06 2.29 5.585 5.25 5.954V17.5h-1.5a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-1.5v-1.546A6.001 6.001 0 0016 10v-.357a.75.75 0 00-1.5 0V10a4.5 4.5 0 01-9 0v-.357z"></path>
               </svg>
-            </motion.div>
+            </button>
           )}
         </div>
+        {isRecording && (
+          <motion.div
+            animate={{ rotate: [0, 360] }}
+            transition={{
+              duration: 4,
+              repeat: Infinity,
+            }}
+            className="pointer-events-none absolute text-pink-500 top-4 right-5 w-6 h-6 rounded-full"
+          >
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M8.34 1.804A1 1 0 019.32 1h1.36a1 1 0 01.98.804l.295 1.473c.497.144.971.342 1.416.587l1.25-.834a1 1 0 011.262.125l.962.962a1 1 0 01.125 1.262l-.834 1.25c.245.445.443.919.587 1.416l1.473.294a1 1 0 01.804.98v1.361a1 1 0 01-.804.98l-1.473.295a6.95 6.95 0 01-.587 1.416l.834 1.25a1 1 0 01-.125 1.262l-.962.962a1 1 0 01-1.262.125l-1.25-.834a6.953 6.953 0 01-1.416.587l-.294 1.473a1 1 0 01-.98.804H9.32a1 1 0 01-.98-.804l-.295-1.473a6.957 6.957 0 01-1.416-.587l-1.25.834a1 1 0 01-1.262-.125l-.962-.962a1 1 0 01-.125-1.262l.834-1.25a6.957 6.957 0 01-.587-1.416l-1.473-.294A1 1 0 011 10.68V9.32a1 1 0 01.804-.98l1.473-.295c.144-.497.342-.971.587-1.416l-.834-1.25a1 1 0 01.125-1.262l.962-.962A1 1 0 015.38 3.03l1.25.834a6.957 6.957 0 011.416-.587l.294-1.473zM13 10a3 3 0 11-6 0 3 3 0 016 0z"
+              ></path>
+            </svg>
+          </motion.div>
+        )}
       </div>
     </form>
   );
