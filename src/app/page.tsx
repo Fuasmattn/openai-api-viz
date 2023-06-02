84969091b65bc32c678e58fa33db4f49c084d168
Martin Prinz <martin.prinz@maibornwolff.de>
2023-06-02 22:50:45 +0200

    chore: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,23 +27,9 @@ export default function Landing() {
       url: "/default.jpg",
       prompt: "Colorful cartoon portrait of stevie wonder",
     },
-    {
-      url: "/default.jpg",
-      prompt: "Colorful cartoon portrait of stevie wonder",
-    },
-    {
-      url: "/default.jpg",
-      prompt: "Colorful cartoon portrait of stevie wonder",
-    },
-    {
-      url: "/default.jpg",
-      prompt: "Colorful cartoon portrait of stevie wonder",
-    },
   ]);
 
   const thumbs = urlList.length === 1 ? [] : urlList;
-  const gridCols = urlList.length;
-  const gridRows = (urlList.length % 5) * gridCols;
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -116,7 +102,7 @@ export default function Landing() {
 
   return (
     <main className="dark:bg-slate-900 bg-slate-100 min-h-screen py-16 flex justify-center">
-      <div className="px-10">
+      <div className="px-10 w-4/5 max-w-3xl">
         <h2 className="dark:text-white text-2xl mb-10 text-center">
           Create your poster
         </h2>
@@ -138,15 +124,15 @@ export default function Landing() {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          className={`relative mt-10 grid grid-cols-${gridCols} grid-rows-${gridRows} gap-2 justfy-center`}
+          className={`relative mt-10 grid grid-cols-8 gap-2 justfy-center`}
         >
           {thumbs.map((item) => (
             <Image
               key={item.url + "thumb"}
               className="rounded-lg hover:cursor-pointer hover:shadow-lg brightness-75 hover:brightness-100 transition-all"
               src={item.url}
-              width={128}
-              height={128}
+              width={1000}
+              height={1000}
               alt={item.prompt}
               onClick={() => {
                 setImageUrl(item.url);
@@ -154,21 +140,21 @@ export default function Landing() {
               }}
             />
           ))}
+          {Array.from(Array(100 - thumbs.length)).map((item) => (
+            <div
+              key={item}
+              className="rounded-lg dark:bg-slate-800 bg-slate-200 h-[80px] w-auto"
+            />
+          ))}
 
           {imageUrl && (
             <Image
-              className={`col-span-${Math.max(
-                1,
-                gridCols - 2
-              )} row-span-${Math.max(0, gridRows - 2)} col-start-${Math.max(
-                1,
-                gridCols - 2
-              )} row-start-${Math.max(0, gridRows - 2)} rounded-lg ${
+              className={`col-span-5 row-span-5 col-start-3 row-start-2 rounded-lg ${
                 isLoading && "animate-pulse"
               }`}
               src={imageUrl}
-              width={400}
-              height={512}
+              width={1000}
+              height={1000}
               onLoadingComplete={onLoadingComplete}
               alt={prompt ?? "generated poster image"}
             />
