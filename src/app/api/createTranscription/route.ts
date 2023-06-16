ddcb25882a1f2600b89c0e16dc930051e4c5ec9e
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-06-16 08:28:51 +0200

    Fix Streaming Stuff

--- src/app/api/createTranscription/route.ts
+++ src/app/api/createTranscription/route.ts
@@ -1,45 +1,16 @@
-/* eslint-disable import/no-anonymous-default-export */
-import multer, { diskStorage } from 'multer';
-import type { NextApiRequest, NextApiResponse, PageConfig } from 'next';
-import fs, { createWriteStream, writeFile, writeFileSync } from 'fs';
 import { OpenAIApi } from 'openai';
 import { configuration } from '../../../config/openai';
 import { NextRequest, NextResponse } from 'next/server';
 import { Readable } from 'stream';
+import type { ReadableStream } from 'node:stream/web';
 
 const openai = new OpenAIApi(configuration);
 
-const upload = multer({
-  storage: diskStorage({
-    destination: function (req, file, callback) {
-      callback(null, './uploads');
-    },
-    filename: function (req, file, callback) {
-      callback(null, file.fieldname + '.mp3');
-    },
-  }),
-});
-
-const middleware = upload.single('audio');
-
-// const fn = async (req: any, res: any) => {
-//   const temp = await req.formData();
-//   const file = temp.get("audio");
-
-//   return new Promise((resolve) => {
-//     middleware(req, res, async () => {
-//       const response = await openai.createTranscription(file, "whisper-1");
-//       resolve({ text: response.data.text });
-//     });
-//   });
-// };
-
 export const POST = async (req: NextRequest): Promise<NextResponse> => {
-  const temp = await req.formData();
-  const file = temp.get('audio');
+  const file = (await req.formData()).get('audio') as File;
 
   const response = openai.createTranscription(
-    Readable.fromWeb((file as any).stream()) as any as File,
+    Readable.fromWeb(file!.stream() as ReadableStream) as unknown as File,
     'whisper-1'
   );
 
